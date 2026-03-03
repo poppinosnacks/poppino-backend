7259afb1458effb621423f71958ffa32fdb63def
learnifyeduverse <learnify.eduverse@gmail.com>
2026-03-03 11:08:55 +0530

    aevii

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -1,30 +1,27 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,          // IMPORTANT
-  secure: false,      // false for 587
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
-  },
-  tls: {
-    rejectUnauthorized: false
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS
   }
 });
 
 module.exports = async (to, subject, text) => {
   try {
     const info = await transporter.sendMail({
-      from: process.env.MAIL_USER,
+      from: "poppinosnacks@gmail.com",
       to,
       subject,
       text
     });
 
-    console.log("Email sent:", info.response);
+    console.log("Brevo email sent:", info.response);
   } catch (err) {
-    console.error("Mail error:", err);
+    console.error("Brevo mail error:", err);
     throw err;
   }
 };
