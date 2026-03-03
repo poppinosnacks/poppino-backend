040ea3641c07bcb32d8574ee97f412c33c2e303a
learnifyeduverse <learnify.eduverse@gmail.com>
2026-03-03 10:45:29 +0530

    fir SMPT port

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -1,18 +1,30 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,          // IMPORTANT
+  secure: false,      // false for 587
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
 module.exports = async (to, subject, text) => {
-  await transporter.sendMail({
-    from: process.env.MAIL_USER,
-    to,
-    subject,
-    text
-  });
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.MAIL_USER,
+      to,
+      subject,
+      text
+    });
+
+    console.log("Email sent:", info.response);
+  } catch (err) {
+    console.error("Mail error:", err);
+    throw err;
+  }
 };
